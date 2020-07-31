0c4926ceb0f2a44b078f65967a14459878848675
TXJ <txj@sd-119654.dedibox.fr>
2020-07-31 14:24:29 +0200

    dont need these anymore

--- src/func.js
+++
@@ -1,9 +0,0 @@
-const { deleteVideo } = require('./config.json'); //Include the current config and pull deleteVideo
-
-module.exports = (client) = {
-  checkManagePermissions(msg, client){
-    if(!msg.channel.permissionsFor(client.user.id).has(`MANAGE_MESSAGES`) && deleteVideo === true){
-      msg.author.send(`Sorry about that, I don't have permissions to delete messages, please give me \`MANAGE_MESSAGES\` permissions in this channel to fix this.`);
-    }
-  }
-};
